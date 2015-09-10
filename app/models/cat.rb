755c97f32356229d7c5002c14dce683f2fc67824
Ian Candy <iancandysemail@gmail.com>
2015-09-10 17:28:42 -0400

    remove solution

--- app/models/cat.rb
+++ app/models/cat.rb
@@ -1,18 +1,1 @@
-class Cat
-
-	ALL_CATS = []
-
-	attr_accessor :name, :color, :breed, :temperament
-
-	def initialize(name, color, breed, temperament)
-		@name = name
-		@color = color
-		@breed = breed
-		@temperament = temperament
-		ALL_CATS << self
-	end
-
-	def self.all
-		ALL_CATS
-	end
-end
+#Your code here!
--- app/models/dog.rb
+++ app/models/dog.rb
@@ -1,18 +1,1 @@
-class Dog
-
-	ALL_DOGS = []
-
-	attr_accessor :name, :color, :breed, :temperament
-
-	def initialize(name, color, breed, temperament)
-		@name = name
-		@color = color
-		@breed = breed
-		@temperament = temperament
-		ALL_DOGS <<
-	end
-
-	def self.all
-		ALL_DOGS
-	end
-end
+#Your code here!
