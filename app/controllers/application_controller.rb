4731f9124d98401af7adac269812da583fef801a
Ian Candy <iancandysemail@gmail.com>
2015-09-10 17:27:42 -0400

    remove solution

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,28 +11,6 @@ class ApplicationController < Sinatra::Base
 		erb :index
 	end
 
-	get "/dogs" do
-		Dog.new("Fido", "Red", "Irish Setter", "Calm")
-		Dog.new("Lassie", "Brown", "Collie", "Helpful")
-		Dog.new("Lance", "Yellow", "Dachsun", "Excited")
-		@dogs = Dog.all
-		erb :dogs
-	end
-
-	get "/cats" do
-		Cat.new("Maru", "Black", "Scottish Fold", "Curious")
-		Cat.new("Stripe", "White", "Tabby", "Annoyed")
-		Cat.new("Rocket", "Orange", "Bobcat", "Calm")
-		@cats = Cat.all
-		erb :cats
-	end
-
-	get "/birds" do
-		Bird.new("Benny", "Blue", "Parrot", "Feisty")
-		Bird.new("Joey", "Red", "Cockatoo", "Funny")
-		Bird.new("Johnny Ramone", "White", "Parakeet", "Calm")
-		@birds = Bird.all
-		erb :birds
-	end
+	## Your code here!
 
 end
